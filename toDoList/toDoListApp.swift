2abf8571066836b2165bbd89a795b36c668fefc1
shreya <shreya.arvnd@gmail.com>
2023-08-01 10:40:55 -0700

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Shreya Aravind on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
